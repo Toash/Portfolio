fb883eb2e9b3dabc8773c3e2b33d81ba76d7934d
Justin <iitoash@gmail.com>
2024-10-13 16:44:24 -0400

    first

---
+++ src/data.ts
@@ -0,0 +1,12 @@
+export const projects = [
+  {
+    link: "https://github.com/Toash/Convolutional-Neural-Network",
+    image: "/drawing_example.gif", //using vite so media should be in public and referenced with root dir
+    title: "Convolutional Neural Network",
+    subtitle: "A program that recognizes images that you draw.",
+    description: "Programmed for a machine learning class",
+  },
+];
+
+// Create seperate section for programming languages
+export const skills = ["React", "AWS", "C#"];
